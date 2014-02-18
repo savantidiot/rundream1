2b7e5a2b8c5142589f33969fa5c70e34b50c1eee
Pradeep <mangalath.pradeep@gmail.com>
2014-02-17 23:13:40 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rundreamapp::Application.config.secret_key_base = '8f3c45688683cc8bee246b30d7de282b4d77fbe21a50c566ff71b7da5160adbb3f36bd825072ea778cef47c7ca08618e2ff5b7f4cdd02b06eb9b2c2bac4a4647'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rundreamapp::Application.config.session_store :cookie_store, key: '_rundreamapp_session'

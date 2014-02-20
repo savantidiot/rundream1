e6a0088de5d7ad5786cafd9aca59588fede14bc5
Pradeep <mangalath.pradeep@gmail.com>
2014-02-19 20:55:33 -0500

    Added user submission form feature

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -4,4 +4,19 @@ class PicsController < ApplicationController
 		@pics = Pic.all
 	end
 
+	def new
+		@pic = Pic.new
+	end
+
+	def create
+		@pic = Pic.create( pic_params )
+		redirect_to pics_path
+	end
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:Name, :Distance, :State)
+	end
+
 end
